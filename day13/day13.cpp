c015eccad925ddfdf221e3cdb1c41a5f1fa6ad01
John Wiltshire <jw@chase.net.au>
2020-12-13 15:35:23 +1000

    Day 13

---
+++ day13/day13.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<int> buses;
+
+    string s;
+    getline(cin, s);
+    int start = stoi(s);
+
+    while (getline(cin, s, ','))
+    {
+        if (s != "x"s) {
+            buses.push_back(stoi(s));
+        }
+        else {
+            buses.push_back(0);
+        }
+    }
+
+    {
+        int t = start;
+        int bus = 0;
+        while (!bus) {
+            for (auto x : buses) {
+                if (x != 0 && t % x == 0) {
+                    bus = x;
+                    break;
+                }
+            }
+            if (!bus) ++t;
+        }
+
+        cout << "Bus = " << bus << " Time = " << t << " Delay = " << (t - start) << " Product = " << (t - start) * bus << endl;
+    }
+
+    {
+        // This could be better.  It assumes primes, and there's no reason to dumb-old increment when you could play with
+        // modulo arithmetic.
+        __int64 t = start;
+        __int64 inc = 1;
+        for (int i = 0; i < buses.size(); ++i) {
+            if (buses[i] == 0) continue;
+            __int64 x = buses[i];
+            while ((t+i) % x != 0) {
+                t += inc;
+            }
+            inc *= x;
+        }
+
+        for (int i = 0; i < buses.size(); ++i) {
+            cout << " Time " << t + i;
+            if (buses[i] != 0) {
+                cout << " Bus = " << buses[i] << " Mod = " << (t + i) % buses[i];
+            }
+            cout << endl;
+        }
+
+        cout << "Magic time = " << t << endl;
+    }
+}
